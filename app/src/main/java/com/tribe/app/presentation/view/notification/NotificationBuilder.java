44cb4b3d877d1d1048502aac982ad99286cde938
Tiago Duarte <duartetia@gmail.com>
2017-05-09 16:27:24 -0400

    Fix for missed call

--- app/src/main/java/com/tribe/app/presentation/view/notification/NotificationBuilder.java
+++ app/src/main/java/com/tribe/app/presentation/view/notification/NotificationBuilder.java
@@ -55,11 +55,6 @@ import javax.inject.Singleton;
   public void sendBundledNotification(RemoteMessage remoteMessage) {
     NotificationPayload notificationPayload = getPayload(remoteMessage);
 
-    if (notificationPayload.getClickAction().equals(NotificationPayload.CLICK_ACTION_END_LIVE)) {
-      if (application.getAppState() != null) {
-        missedCallManager.setMissedNotificationPlayload(notificationPayload);
-      }
-    }
     if (notificationPayload != null && !StringUtils.isEmpty(notificationPayload.getClickAction())) {
       // If the user calling is hidden by the current user, we unhide it
       // https://github.com/heytribe/roadmap/issues/530
@@ -72,6 +67,10 @@ import javax.inject.Singleton;
         }
       } else if (notificationPayload.getClickAction()
           .equals(NotificationPayload.CLICK_ACTION_END_LIVE)) {
+        if (application.getAppState() != null) {
+          missedCallManager.setMissedNotificationPlayload(notificationPayload);
+        }
+
         PreferencesUtils.removeFromSet(fullScreenNotificationState,
             notificationPayload.getThread());
       }
