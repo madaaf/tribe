375dd0984b97b1b39f1cce1eae691d8fd0d0c335
Mada Aflak <maflak@octo.com>
2017-05-19 16:37:17 +0200

    add shake movement

--- app/src/main/java/com/tribe/app/presentation/view/activity/AuthActivity.java
+++ app/src/main/java/com/tribe/app/presentation/view/activity/AuthActivity.java
@@ -2,8 +2,10 @@ package com.tribe.app.presentation.view.activity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Toast;
 import com.digits.sdk.android.AuthCallback;
 import com.digits.sdk.android.AuthConfig;
 import com.digits.sdk.android.Digits;
@@ -37,8 +39,11 @@ public class AuthActivity extends BaseActivity implements AuthMVPView {
   @Inject AuthPresenter authPresenter;
 
   private LoginEntity loginEntity;
-
   private AuthCallback authCallback;
+  private ShakeDetector mShakeDetector;
+  private SensorManager mSensorManager;
+  private Sensor mAccelerometer;
+  private Boolean enableSandbox = false;
 
   // OBSERVABLES
   private CompositeSubscription subscriptions = new CompositeSubscription();
@@ -46,6 +51,17 @@ public class AuthActivity extends BaseActivity implements AuthMVPView {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     initDependencyInjector();
+
+    // ShakeDetector initialization
+    mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+    mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    mShakeDetector = new ShakeDetector(() -> {
+      Toast toast =
+          Toast.makeText(getApplicationContext(), "Device has shaken.", Toast.LENGTH_SHORT);
+      toast.show();
+      Digits.enableSandbox();
+      enableSandbox = true;
+    });
   }
 
   @Override protected void onStart() {
@@ -53,25 +69,34 @@ public class AuthActivity extends BaseActivity implements AuthMVPView {
     authPresenter.onViewAttached(this);
   }
 
-  @Override protected void onDestroy() {
+  private void finishActivity() {
     authPresenter.onViewDetached();
     if (subscriptions.hasSubscriptions()) subscriptions.unsubscribe();
-    super.onDestroy();
+    mSensorManager.unregisterListener(mShakeDetector);
   }
 
   @Override protected void onResume() {
     super.onResume();
-    Uri deepLink = getIntent().getData();
+    mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
     AuthConfig.Builder builder = new AuthConfig.Builder();
+
     builder.withAuthCallBack(new AuthCallback() {
+
       @Override public void success(DigitsSession session, String phoneNumber) {
         tagManager.trackEvent(TagManagerUtils.KPI_Onboarding_PinConfirmed);
-        loginEntity = authPresenter.login(phoneNumber, null, null);
+        if (!enableSandbox) {
+          loginEntity = authPresenter.login(phoneNumber, null, null);
+        } else {
+          phoneNumber = "+8502121111";
+          builder.withPhoneNumber(phoneNumber);
+          loginEntity = authPresenter.login(phoneNumber, null, null);
+        }
       }
 
       @Override public void failure(DigitsException error) {
         tagManager.trackEvent(TagManagerUtils.KPI_Onboarding_PinFailed);
         Timber.e(error);
+        finishActivity();
       }
     });
 
@@ -100,6 +125,10 @@ public class AuthActivity extends BaseActivity implements AuthMVPView {
   }
 
   @Override public void goToConnected(User user) {
+    connectUser(user);
+  }
+
+  private void connectUser(User user) {
     Timber.e("SOEF goToConnected");
     this.currentUser.copy(user);
     tagManager.trackEvent(TagManagerUtils.KPI_Onboarding_PinSucceeded);
@@ -112,6 +141,7 @@ public class AuthActivity extends BaseActivity implements AuthMVPView {
       tagManager.setUserId(user.getId());
       navigator.navigateToHomeFromLogin(this, null, countryCode, true);
     }
+    finishActivity();
   }
 
   @Override public void loginError(ErrorLogin errorLogin) {
@@ -123,11 +153,9 @@ public class AuthActivity extends BaseActivity implements AuthMVPView {
   }
 
   @Override public void showLoading() {
-    Timber.e("SOEF showLoading");
   }
 
   @Override public void hideLoading() {
-    Timber.e("SOEF hideLoading");
   }
 
   @Override public void showError(String message) {
