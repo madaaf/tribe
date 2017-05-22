311ed752b60536c23b486267d7c5f77e7497b0f6
Mada Aflak <maflak@octo.com>
2017-05-22 10:04:24 +0200

    add test users

--- app/src/main/java/com/tribe/app/presentation/view/activity/AuthActivity.java
+++ app/src/main/java/com/tribe/app/presentation/view/activity/AuthActivity.java
@@ -1,5 +1,7 @@
 package com.tribe.app.presentation.view.activity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
@@ -24,15 +26,16 @@ import com.tribe.app.domain.entity.User;
 import com.tribe.app.presentation.internal.di.components.DaggerUserComponent;
 import com.tribe.app.presentation.mvp.presenter.AuthPresenter;
 import com.tribe.app.presentation.mvp.view.AuthMVPView;
+import com.tribe.app.presentation.utils.IntentUtils;
 import com.tribe.app.presentation.utils.StringUtils;
 import com.tribe.app.presentation.utils.analytics.TagManagerUtils;
+import com.tribe.app.presentation.utils.facebook.FacebookUtils;
 import com.tribe.app.presentation.view.utils.PhoneUtils;
 import com.tribe.app.presentation.view.utils.ShakeDetector;
 import javax.inject.Inject;
 import timber.log.Timber;
 
 public class AuthActivity extends BaseActivity implements AuthMVPView {
-
   public static Intent getCallingIntent(Context context) {
     Intent intent = new Intent(context, AuthActivity.class);
     return intent;
@@ -53,6 +56,7 @@ public class AuthActivity extends BaseActivity implements AuthMVPView {
   private SensorManager mSensorManager;
   private Sensor mAccelerometer;
   private Boolean enableSandbox = false;
+  private AuthCallback authCallback;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -61,6 +65,7 @@ public class AuthActivity extends BaseActivity implements AuthMVPView {
     initDependencyInjector();
     setSandboxBehavior();
     initRessource();
+    digitAuth();
   }
 
   ////////////////
@@ -68,9 +73,7 @@ public class AuthActivity extends BaseActivity implements AuthMVPView {
   ////////////////
 
   private void digitAuth() {
-    AuthConfig.Builder builder = new AuthConfig.Builder();
-    builder.withAuthCallBack(new AuthCallback() {
-
+    authCallback = new AuthCallback() {
       @Override public void success(DigitsSession session, String phoneNumber) {
         tagManager.trackEvent(TagManagerUtils.KPI_Onboarding_PinConfirmed);
         if (!enableSandbox) {
@@ -80,6 +83,7 @@ public class AuthActivity extends BaseActivity implements AuthMVPView {
         } else {
           Toast toast = Toast.makeText(getApplicationContext(), "PIN ERROR", Toast.LENGTH_SHORT);
           toast.show();
+          logout();
         }
       }
 
@@ -87,10 +91,33 @@ public class AuthActivity extends BaseActivity implements AuthMVPView {
         tagManager.trackEvent(TagManagerUtils.KPI_Onboarding_PinFailed);
         Timber.e(error);
       }
-    });
+    };
 
+    AuthConfig.Builder builder = new AuthConfig.Builder();
+    builder.withAuthCallBack(authCallback);
     AuthConfig authConfig = builder.build();
     Digits.authenticate(authConfig);
+  }
+  
+  private void logout() {
+    FacebookUtils.logout();
+    Digits.logout();
+    Intent intent = new Intent(this, HomeActivity.class);
+    intent.putExtra(IntentUtils.FINISH, true);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    startActivity(intent);
+
+    Intent intentLauncher = new Intent(this, LauncherActivity.class);
+    intentLauncher.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+    intentLauncher.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+    intentLauncher.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+    int pendingIntentId = 123456; // FAKE ID
+    PendingIntent mPendingIntent = PendingIntent.getActivity(this, pendingIntentId, intentLauncher,
+        PendingIntent.FLAG_CANCEL_CURRENT);
+    AlarmManager mgr = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+    mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 100, mPendingIntent);
+    System.exit(0);
   }
 
   private void setSandboxBehavior() {
@@ -150,7 +177,6 @@ public class AuthActivity extends BaseActivity implements AuthMVPView {
 
   @Override protected void onResume() {
     super.onResume();
-    digitAuth();
   }
 
   @Override protected void onStart() {
