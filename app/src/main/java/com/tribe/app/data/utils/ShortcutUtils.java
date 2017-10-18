89aa915cfc90f224a41fd9ed8f42b9676f07451d
Tiago Duarte <duartetia@gmail.com>
2017-10-18 17:22:50 +0200

    Added sort for hash shortcut

--- app/src/main/java/com/tribe/app/data/utils/ShortcutUtils.java
+++ app/src/main/java/com/tribe/app/data/utils/ShortcutUtils.java
@@ -1,5 +1,7 @@
 package com.tribe.app.data.utils;
 
+import java.util.Arrays;
+
 /**
  * Created by tiago on 18/10/2017.
  */
@@ -7,6 +9,7 @@ package com.tribe.app.data.utils;
 public class ShortcutUtils {
 
   public static String hashShortcut(String excludeId, String[] memberIds) {
+    Arrays.sort(memberIds);
     long result = 17;
     for (String id : memberIds) if (!id.equals(excludeId)) result = 37 * result + id.hashCode();
     return String.valueOf(result);
