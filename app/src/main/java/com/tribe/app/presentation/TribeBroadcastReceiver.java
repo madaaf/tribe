2ed80568de6afdd446051f3b12e802e82dd89974
Mada Aflak <maflak@octo.com>
2017-11-29 15:53:12 -0800

    [Notifications] Add action on Chat related in-app notifications #1191

--- app/src/main/java/com/tribe/app/presentation/TribeBroadcastReceiver.java
+++ app/src/main/java/com/tribe/app/presentation/TribeBroadcastReceiver.java
@@ -5,7 +5,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Pair;
-import android.view.View;
 import com.tribe.app.R;
 import com.tribe.app.domain.entity.Invite;
 import com.tribe.app.domain.entity.Room;
