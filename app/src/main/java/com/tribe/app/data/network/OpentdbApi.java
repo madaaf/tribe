7fc407f0ee91253d30776bfa85f9c670b632ce46
Tiago Duarte <duartetia@gmail.com>
2017-12-21 11:23:15 +0100

    Added trivia questions

--- app/src/main/java/com/tribe/app/data/network/OpentdbApi.java
+++ app/src/main/java/com/tribe/app/data/network/OpentdbApi.java
@@ -1,18 +1,14 @@
 package com.tribe.app.data.network;
 
-import com.tribe.tribelivesdk.game.trivia.TriviaCategoryEnum;
+import com.tribe.tribelivesdk.game.trivia.TriviaQuestions;
 import java.util.List;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
-import retrofit2.http.Streaming;
 import retrofit2.http.Url;
 import rx.Observable;
 
 public interface OpentdbApi {
 
-  @GET("/api.php?type=multiple&encode=url3986") Observable<TriviaCategoryEnum> getCategory(@Query("category") int category);
-
-  @GET Observable<List<String>> getDataForUrl(@Url String fileUrl);
+  @GET("/api.php?type=multiple&encode=url3986") Observable<TriviaQuestions> getCategory(
+      @Query("category") int category);
 }
--- tribelivesdk/src/main/java/com/tribe/tribelivesdk/game/trivia/TriviaQuestions.java
+++ tribelivesdk/src/main/java/com/tribe/tribelivesdk/game/trivia/TriviaQuestions.java
@@ -1,10 +1,47 @@
 package com.tribe.tribelivesdk.game.trivia;
 
+import java.util.List;
+
 /**
  * Created by tiago on 21/12/2017.
  */
 
 public class TriviaQuestions {
 
+  private String id;
+  private String question;
+  private String answer;
+  private List<String> alternativeAnswers;
 
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getQuestion() {
+    return question;
+  }
+
+  public void setQuestion(String question) {
+    this.question = question;
+  }
+
+  public String getAnswer() {
+    return answer;
+  }
+
+  public void setAnswer(String answer) {
+    this.answer = answer;
+  }
+
+  public List<String> getAlternativeAnswers() {
+    return alternativeAnswers;
+  }
+
+  public void setAlternativeAnswers(List<String> alternativeAnswers) {
+    this.alternativeAnswers = alternativeAnswers;
+  }
 }
