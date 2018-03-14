bc39aa8767fc13ea237979d341d5e82e2fea91a3
Tiago Duarte <duartetia@gmail.com>
2018-03-14 14:52:40 -0700

    Init cool cams

---
+++ app/src/main/java/com/tribe/app/presentation/view/component/live/game/coolcams/GameCoolCamsView.java
@@ -0,0 +1,217 @@
+package com.tribe.app.presentation.view.component.live.game.coolcams;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.constraint.Group;
+import android.support.transition.ChangeBounds;
+import android.support.transition.Transition;
+import android.support.transition.TransitionListenerAdapter;
+import android.support.transition.TransitionManager;
+import android.support.v4.widget.TextViewCompat;
+import android.text.Html;
+import android.util.AttributeSet;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.OvershootInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.tribe.app.R;
+import com.tribe.app.domain.entity.trivia.TriviaCategoryEnum;
+import com.tribe.app.domain.entity.trivia.TriviaQuestion;
+import com.tribe.app.presentation.internal.di.components.DaggerUserComponent;
+import com.tribe.app.presentation.mvp.presenter.GamePresenter;
+import com.tribe.app.presentation.mvp.view.adapter.GameMVPViewAdapter;
+import com.tribe.app.presentation.utils.EmojiParser;
+import com.tribe.app.presentation.utils.FontUtils;
+import com.tribe.app.presentation.view.component.live.LiveStreamView;
+import com.tribe.app.presentation.view.component.live.game.common.GameAnswerView;
+import com.tribe.app.presentation.view.component.live.game.common.GameAnswersView;
+import com.tribe.app.presentation.view.component.live.game.common.GameViewWithRanking;
+import com.tribe.app.presentation.view.component.live.game.trivia.GameTriviaCategoryView;
+import com.tribe.app.presentation.view.component.live.game.trivia.GameTriviaView;
+import com.tribe.app.presentation.view.utils.AnimationUtils;
+import com.tribe.app.presentation.view.utils.DialogFactory;
+import com.tribe.app.presentation.view.utils.SoundManager;
+import com.tribe.app.presentation.view.widget.CircularProgressBar;
+import com.tribe.app.presentation.view.widget.TextViewFont;
+import com.tribe.tribelivesdk.game.Game;
+import com.tribe.tribelivesdk.model.TribeGuest;
+import com.tribe.tribelivesdk.model.TribeSession;
+import com.tribe.tribelivesdk.util.JsonUtils;
+import com.tribe.tribelivesdk.util.ObservableRxHashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import javax.inject.Inject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import rx.Observable;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+
+/**
+ * Created by tiago on 14/03/2018.
+ */
+
+public class GameCoolCamsView extends GameViewWithRanking {
+
+  private static final String ACTION_POSITION = "position";
+  private static final String ACTION_NEW_GAME = "newGame";
+  private static final String ACTION_USER_GAME_OVER = "userGameOver";
+  private static final String ACTION_GAME_OVER = "gameOver";
+
+  private static final String STEPS_IDS_KEY = "position";
+  private static final String STEP_DURATION = "position";
+  private static final String STEP_RESULT_DURATION = "position";
+  private static final String TIMESTAMP_KEY = "position";
+  private static final String STATUS_CODE_KEY = "position";
+  private static final String STEP_ID_KEY = "position";
+
+  private static final String X_KEY = "x";
+  private static final String Y_KEY = "y";
+
+  private static final String ACTION_STOP_TRACK = "stopTrack";
+  private static final String ACTION_PLAY_TRACK = "playTrack";
+  private static final String TRACK_KEY = "track";
+  private static final String WINNERS_KEY = "winners";
+
+  @Inject GamePresenter gamePresenter;
+
+  @BindView(R.id.layoutConstraint) ConstraintLayout layoutConstraint;
+
+  // VARIABLES
+  private GameMVPViewAdapter gameMVPViewAdapter;
+
+  // SUBSCRIPTIONS
+
+  public GameCoolCamsView(@NonNull Context context) {
+    super(context);
+  }
+
+  public GameCoolCamsView(@NonNull Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  @Override protected void onAttachedToWindow() {
+    super.onAttachedToWindow();
+    gamePresenter.onViewAttached(gameMVPViewAdapter);
+  }
+
+  @Override protected void onDetachedFromWindow() {
+    super.onDetachedFromWindow();
+    gamePresenter.onViewDetached();
+  }
+
+  @Override protected void initDependencyInjector() {
+    DaggerUserComponent.builder()
+        .activityModule(getActivityModule())
+        .applicationComponent(getApplicationComponent())
+        .build()
+        .inject(this);
+  }
+
+  @Override protected void initView(Context context) {
+    super.initView(context);
+
+    inflater.inflate(R.layout.view_game_trivia_init, this, true);
+    unbinder = ButterKnife.bind(this);
+
+    setClickable(true);
+
+    gameMVPViewAdapter = new GameMVPViewAdapter() {
+
+    };
+  }
+
+  @Override protected void initWebRTCRoomSubscriptions() {
+    super.initWebRTCRoomSubscriptions();
+    subscriptionsRoom.add(webRTCRoom.onGameMessage()
+        .onBackpressureDrop()
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(pair -> {
+          JSONObject jsonObject = pair.second;
+          TribeSession tribeSession = pair.first;
+
+          if (jsonObject.has(game.getId())) {
+            try {
+              JSONObject message = jsonObject.getJSONObject(game.getId());
+              receiveMessage(tribeSession, message);
+            } catch (JSONException e) {
+              e.printStackTrace();
+            }
+          }
+        }));
+  }
+
+  @Override protected void takeOverGame() {
+
+  }
+
+  @Override protected void receiveMessage(TribeSession tribeSession, JSONObject jsonObject) {
+    if (jsonObject.has(game.getId())) {
+      try {
+        JSONObject message = jsonObject.getJSONObject(game.getId());
+
+      } catch (JSONException ex) {
+        ex.printStackTrace();
+      }
+    }
+
+    super.receiveMessage(tribeSession, jsonObject);
+  }
+
+  /**
+   * JSON PAYLOAD
+   */
+
+  /**
+   * PUBLIC
+   */
+
+  public void initSubscriptions() {
+
+  }
+
+  @Override public void start(Game game,
+      Observable<ObservableRxHashMap.RxHashMap<String, TribeGuest>> masterMapObs,
+      Observable<Map<String, TribeGuest>> mapObservable,
+      Observable<Map<String, TribeGuest>> mapInvitedObservable,
+      Observable<Map<String, LiveStreamView>> liveViewsObservable, String userId) {
+    super.start(game, masterMapObs, mapObservable, mapInvitedObservable, liveViewsObservable,
+        userId);
+
+    currentMasterId = userId;
+    game.setCurrentMaster(peerMap.get(currentMasterId));
+  }
+
+  @Override public void stop() {
+    super.stop();
+  }
+
+  @Override public void dispose() {
+    super.dispose();
+  }
+
+  @Override public void setNextGame() {
+
+  }
+
+  /**
+   * OBSERVABLES
+   */
+}
