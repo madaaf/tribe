ebb7023b5dbac1b6643b945bea4e255c1faf75af
Mada Aflak <maflak@octo.com>
2018-04-10 18:19:41 -0700

    [Live] Improve Loading game.. step #1515

--- app/src/main/java/com/tribe/app/presentation/view/widget/LoadingGameView.java
+++ app/src/main/java/com/tribe/app/presentation/view/widget/LoadingGameView.java
@@ -1,7 +1,5 @@
 package com.tribe.app.presentation.view.widget;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.annotation.NonNull;
@@ -11,11 +9,14 @@ import android.view.LayoutInflater;
 import android.view.animation.OvershootInterpolator;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import com.tribe.app.R;
 import com.tribe.app.presentation.AndroidApplication;
+import com.tribe.app.presentation.view.utils.ScreenUtils;
+import javax.inject.Inject;
 
 /**
  * Created by madaaflak on 02/04/2018.
@@ -27,9 +28,13 @@ public class LoadingGameView extends FrameLayout {
   private LayoutInflater inflater;
   private Unbinder unbinder;
   private ValueAnimator animator;
+  private OnFinishEventListener listener;
 
   @BindView(R.id.loading_game_back) ImageView background;
   @BindView(R.id.loading_game_btn) ImageView btns;
+  @BindView(R.id.container) RelativeLayout container;
+
+  @Inject ScreenUtils screenUtils;
 
   public LoadingGameView(@NonNull Context context) {
     super(context);
@@ -44,31 +49,64 @@ public class LoadingGameView extends FrameLayout {
   private void initView(Context context) {
     inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     inflater.inflate(R.layout.view_loading_game, this, true);
-    unbinder = ButterKnife.bind(this);
     ((AndroidApplication) getContext().getApplicationContext()).getApplicationComponent()
         .inject(this);
-    setAlpha(0);
+    unbinder = ButterKnife.bind(this);
+    setAlpha(1);
   }
 
   public void setAnim(float a, float b) {
-    setAlpha(1f);
-    if (animator == null) {
-      animator = ValueAnimator.ofFloat(a, b);
-      animator.setInterpolator(new OvershootInterpolator(1.5f));
-      animator.setStartDelay(500);
-      animator.addUpdateListener(animation -> {
-        float value = (float) animation.getAnimatedValue();
-        background.setRotation(value);
-        btns.setRotation(-value);
-      });
-      animator.setDuration(500);
-      animator.addListener(new AnimatorListenerAdapter() {
-        @Override public void onAnimationEnd(Animator animation) {
-          super.onAnimationEnd(animation);
-          setAnim(a + 90f, b + 90f);
-        }
-      });
-      animator.start();
+    if (animator != null) {
+      return;
     }
+    setAlpha(1f);
+    animator = ValueAnimator.ofFloat(a, b);
+    animator.setInterpolator(new OvershootInterpolator(1.2f));
+    animator.setStartDelay(500);
+    animator.setRepeatCount(ValueAnimator.INFINITE);
+    animator.addUpdateListener(animation -> {
+      float value = (float) animation.getAnimatedValue();
+      background.setRotation(value);
+      btns.setRotation(-value);
+    });
+    animator.setDuration(1000);
+    animator.start();
+  }
+
+  boolean isFinish = false;
+
+  public void finish() {
+    if (isFinish) {
+      isFinish = false;
+      container.animate()
+          .translationX(screenUtils.getWidthPx())
+          .setDuration(3000)
+          .withEndAction(new Runnable() {
+            @Override public void run() {
+              if (listener != null) listener.onFinishAnim();
+            }
+          })
+          .setInterpolator(new OvershootInterpolator(1.5f))
+          .start();
+    }
+  }
+
+  public void start() {
+    isFinish = true;
+    container.setTranslationX(-screenUtils.getWidthPx());
+    container.animate()
+        .setStartDelay(500)
+        .translationX(0)
+        .setDuration(1000)
+        .setInterpolator(new OvershootInterpolator(1.2f))
+        .start();
+  }
+
+  public void setNotifEventListener(OnFinishEventListener eventListener) {
+    listener = eventListener;
+  }
+
+  public interface OnFinishEventListener {
+    void onFinishAnim();
   }
 }
